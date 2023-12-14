595543426f342b251e848514d8748cbc8c9a715b
haydar-ali <haidar.ali@outlook.co.id>
2023-12-14 22:52:40 +0700

    add auth pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import logo from "./logo.svg";
-import { Container, PageItem } from "react-bootstrap";
-import Menu from "./components/Menu";
 import "bootstrap/dist/css/bootstrap.min.css";
-import CarouseSlider from "./components/CarouselSlider";
-import Diskon from "./components/Diskon";
-import Product from "./components/Product";
-import PaginationSetup from "./components/PaginationSetup";
-import Footer from "./components/Footer";
 
+//import routes
+import Home from './Home';
 function App() {
   return (
     <>
-      <Menu />
-      <Container>
-        <CarouseSlider />
-        <Diskon />
-        <Product />
-        <PaginationSetup />
-      </Container>
-      <Footer />
+        <Home/>
+
     </>
   );
 }
